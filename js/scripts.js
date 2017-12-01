a036a32fb63bacfea103085cc21951f1c234d790
fdarius2014 <fdarius2014@my.fit.edu>
2017-12-01 16:03:16 -0600

    Clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,3 @@
-/*
-   
-    Template Name : Rolling - Freelancer Portfolio Template
-    Author : UiPasta Team
-    Website : http://www.uipasta.com/
-    Support : http://www.uipasta.com/support/
-	
-	
-*/
-
-
-
-/*
-   
-   Table Of Content
-   
-   1. Preloader
-   2. Smooth Scroll
-   3. Scroll Naviagation Background Change with Sticky Navigation
-   4. Mobile Navigation Hide or Collapse on Click
-   5. Scroll To Top
-   6. Typed.js
-   7. Parallax Background
-   8. Portfolio Filtering
-   9. Magnific Popup
-  10. Testimonial Carousel/Slider
-  11. Statistics Counter
-  12. Google Map
- 
-
-*/
-
 
 (function ($) {
     'use strict';
@@ -110,7 +78,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Software Engineer.", "I Live in Texas.", "I am passionate about Web Development and Machine Learning."],    /* You can change the home section typing text from
+            strings: ["I am A Software Engineer.", "I am passionate about Web Development and Machine Learning."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
